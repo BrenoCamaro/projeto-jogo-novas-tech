d2fc15b8b02e6e06ed93652da1a5e585b88e7434
BrenoCamaro <breno.camaro.5@gmail.com>
2024-11-04 16:42:02 -0300

    #3: Implementando os movimentos do retangulo, ou seja, do personagem.

--- main.py
+++ main.py
@@ -11,14 +11,27 @@ largura = 640
 altura = 480
 tela = pygame.display.set_mode((largura, altura))
 jogador = Jogador.Jogador()
-imagem_personagem_parado = pygame.sprite.Group()
-imagem_personagem_parado.add(jogador)
+x = int(largura / 2)
+y = int(altura / 2)
+relogio = pygame.time.Clock()
 
 # Game-Loop
 while True:
+    relogio.tick(30)
+    tela.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    imagem_personagem_parado.draw(tela)
+
+        if pygame.key.get_pressed()[K_a]: 
+            x = x - 20
+        if pygame.key.get_pressed()[K_d]:
+            x = x + 20
+        if pygame.key.get_pressed()[K_w]:
+            y = y - 20
+        if pygame.key.get_pressed()[K_s]:
+            y = y + 20
+                
+    rect_vermelho = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
     pygame.display.flip()
