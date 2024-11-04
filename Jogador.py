4c461a4664c4d049d531c81c6635fb411ed3f791
BrenoCamaro <breno.camaro.5@gmail.com>
2024-11-04 14:37:53 -0300

    #2: Desenvolvendo a classe Jogador.py e desenhando jogador na tela

--- Jogador.py
+++ Jogador.py
@@ -1,3 +1,17 @@
 # Bibliotecas utilizadas 
 import pygame
 from pygame.locals import *
+import os
+
+class Jogador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        pasta_imagens_jogador = "imagens_jogador"
+        nome_imagens_jogador_parado = ["personagem_parado1.png", "personagem_parado2.png", "personagem_parado3.png", "personagem_parado4.png", "personagem_parado5.png"]
+        self.sprite_parado = [pygame.image.load(os.path.join(pasta_imagens_jogador, nome)) for nome in nome_imagens_jogador_parado]
+        
+        self.image = self.sprite_parado[0]
+        self.rect = self.image.get_rect() # Pegando o retangulo da imagem
+        self.rect.topleft = 100, 100 # Posição da imagem na tela
+
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+import Jogador
 
 pygame.init()
 
@@ -9,6 +10,9 @@ pygame.init()
 largura = 640
 altura = 480
 tela = pygame.display.set_mode((largura, altura))
+jogador = Jogador.Jogador()
+imagem_personagem_parado = pygame.sprite.Group()
+imagem_personagem_parado.add(jogador)
 
 # Game-Loop
 while True:
@@ -16,4 +20,5 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+    imagem_personagem_parado.draw(tela)
     pygame.display.flip()
