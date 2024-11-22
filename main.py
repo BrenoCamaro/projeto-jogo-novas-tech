3ad30df6fa4f9ce92d659218e929aa72a54cc58d
BrenoCamaro <breno.camaro.5@gmail.com>
2024-11-22 11:08:55 -0300

    criando a animação do plano de fundo

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ pygame.init()
 LARGURA = 1000
 ALTURA = 667
 TELA = pygame.display.set_mode((LARGURA, ALTURA))
+IMAGEM_FUNDO = pygame.image.load("imagens/PlanoDeFundo.jpg").convert_alpha()
+IMAGEM_FUNDO = pygame.transform.scale(IMAGEM_FUNDO, (LARGURA, ALTURA))
 pygame.display.set_caption("Space Journey")
+
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    TELA.blit(IMAGEM_FUNDO, (0, 0))
+
+    #Criando movimento da tela de fundo
+    rel_x = LARGURA % IMAGEM_FUNDO.get_rect().width
+    TELA.blit(IMAGEM_FUNDO, (rel_x - IMAGEM_FUNDO.get_rect().width, 0))
+    if rel_x < LARGURA:
+        TELA.blit(IMAGEM_FUNDO, (rel_x, 0))
+    LARGURA -=1
+
     pygame.display.update()
             
