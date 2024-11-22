7efb13e8b1182d66190c9c672bf5fe60387f9e4d
BrenoCamaro <breno.camaro.5@gmail.com>
2024-11-22 11:20:07 -0300

    Imprimindo jogador e aliens na tela

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ ALTURA = 667
 TELA = pygame.display.set_mode((LARGURA, ALTURA))
 IMAGEM_FUNDO = pygame.image.load("imagens/PlanoDeFundo.jpg").convert_alpha()
 IMAGEM_FUNDO = pygame.transform.scale(IMAGEM_FUNDO, (LARGURA, ALTURA))
+
+ALIEN_IMG = pygame.image.load("imagens/aircraft.png").convert_alpha()
+ALIEN_IMG = pygame.transform.scale(ALIEN_IMG, (50, 50))
+POSICAO_X_ALIEN = 500
+POSICAO_Y_ALIEN = 300
+
+JOGADOR_IMG = pygame.image.load("imagens/spaceship (1).png").convert_alpha()
+JOGADOR_IMG = pygame.transform.scale(JOGADOR_IMG, (50, 50))
+JOGADOR_IMG = pygame.transform.rotate(JOGADOR_IMG, -90)
+POSICAO_X_JOGADOR = 200
+POSICAO_Y_JOGADOR = 300
+
 pygame.display.set_caption("Space Journey")
 
 
@@ -24,7 +36,9 @@ while True:
     TELA.blit(IMAGEM_FUNDO, (rel_x - IMAGEM_FUNDO.get_rect().width, 0))
     if rel_x < LARGURA:
         TELA.blit(IMAGEM_FUNDO, (rel_x, 0))
-    LARGURA -=1
+    LARGURA -= 2
 
+    TELA.blit(ALIEN_IMG, (POSICAO_X_ALIEN, POSICAO_Y_ALIEN))
+    TELA.blit(JOGADOR_IMG, (POSICAO_X_JOGADOR, POSICAO_Y_JOGADOR))
     pygame.display.update()
             
