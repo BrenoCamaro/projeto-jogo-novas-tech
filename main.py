8f975926c60c2196cc9e26f38bef3b6a26c6359f
BrenoCamaro <breno.camaro.5@gmail.com>
2024-11-25 16:05:03 -0300

    Desenhando as munições.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 LARGURA = 1000
 ALTURA = 667
 TELA = pygame.display.set_mode((LARGURA, ALTURA))
-IMAGEM_FUNDO = pygame.image.load("imagens/PlanoDeFundo.jpg").convert_alpha()
+IMAGEM_FUNDO = pygame.image.load("imagens/1.jpg").convert_alpha()
 IMAGEM_FUNDO = pygame.transform.scale(IMAGEM_FUNDO, (LARGURA, ALTURA))
 
 ALIEN_IMG = pygame.image.load("imagens/aircraft.png").convert_alpha()
@@ -21,6 +21,17 @@ JOGADOR_IMG = pygame.transform.scale(JOGADOR_IMG, (50, 50))
 JOGADOR_IMG = pygame.transform.rotate(JOGADOR_IMG, -90)
 POSICAO_X_JOGADOR = 200
 POSICAO_Y_JOGADOR = 300
+
+
+MUNICAO = pygame.image.load("imagens/bullet.png").convert_alpha()
+MUNICAO = pygame.transform.scale(MUNICAO, (25, 25))
+MUNICAO = pygame.transform.rotate(MUNICAO, -45)
+
+VELOCIDADE_X_MUNICAO = 0
+POSICAO_X_MUNICAO = 200
+POSICAO_Y_MUNICAO = 300
+
+GATILHO = False
 
 pygame.display.set_caption("Space Journey")
 
@@ -48,8 +59,19 @@ while True:
     TECLA = pygame.key.get_pressed()
     if TECLA[K_w] and POSICAO_Y_JOGADOR > 1:
         POSICAO_Y_JOGADOR -= 5
+
+        if not GATILHO:
+            POSICAO_Y_MUNICAO -= 5
+
     if TECLA[K_s] and POSICAO_Y_JOGADOR < ALTURA:
         POSICAO_Y_JOGADOR += 5
+
+        if not GATILHO:
+            POSICAO_Y_MUNICAO += 5
+
+    if TECLA[K_SPACE]:
+        GATILHO = True
+        VELOCIDADE_X_MUNICAO = 1
 
     #Respawn do Alien
     if POSICAO_X_ALIEN == 50:
@@ -60,7 +82,11 @@ while True:
     LARGURA -= 2
     POSICAO_X_ALIEN -= 1
 
+    #Movimento da Bala
+    POSICAO_X_MUNICAO += VELOCIDADE_X_MUNICAO
+
     TELA.blit(ALIEN_IMG, (POSICAO_X_ALIEN, POSICAO_Y_ALIEN))
+    TELA.blit(MUNICAO, (POSICAO_X_MUNICAO, POSICAO_Y_MUNICAO))
     TELA.blit(JOGADOR_IMG, (POSICAO_X_JOGADOR, POSICAO_Y_JOGADOR))
     pygame.display.update()
             
