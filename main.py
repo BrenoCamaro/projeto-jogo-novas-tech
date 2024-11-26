c4c649425051d2e454320a6a9dcae80e31fbbd03
BrenoCamaro <breno.camaro.5@gmail.com>
2024-11-26 08:19:20 -0300

    Refatorando: Substituindo variáveis globais por atributos da classe Alien

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import  sys
 import random
 from pygame.locals import *
+from alien import Alien
 
 pygame.init()
 
@@ -18,12 +19,14 @@ PONTOS = 2
 pygame.display.set_caption("Space Journey")
 
 #Classe Alien
+'''
 ALIEN_IMG = pygame.image.load("imagens/aircraft.png").convert_alpha()
 ALIEN_IMG = pygame.transform.scale(ALIEN_IMG, (50, 50))
 POSICAO_X_ALIEN = 500
 POSICAO_Y_ALIEN = 300
 ALIEN_IMG_RECT = ALIEN_IMG.get_rect()
-
+'''
+alien = Alien()
 
 #Classe Jogador
 JOGADOR_IMG = pygame.image.load("imagens/spaceship (1).png").convert_alpha()
@@ -61,10 +64,10 @@ def respawn_municao():
 def colisao():
     global PONTOS
 
-    if JOGADOR_IMG_RECT.colliderect(ALIEN_IMG_RECT) or ALIEN_IMG_RECT.x == 60:
+    if JOGADOR_IMG_RECT.colliderect(alien.retanguloDaImagem) or alien.retanguloDaImagem.x == 60:
         PONTOS -= 1
         return True
-    elif MUNICAO_RECT.colliderect(ALIEN_IMG_RECT):
+    elif MUNICAO_RECT.colliderect(alien.retanguloDaImagem):
         PONTOS += 1
         return True
     else:
@@ -106,40 +109,40 @@ while RODANDO:
     if PONTOS == -1:
         RODANDO = False
     #Respawn do Alien
-    if POSICAO_X_ALIEN == 50:
-        POSICAO_X_ALIEN = respawn()[0]
-        POSICAO_Y_ALIEN = respawn()[1]
+    if alien.coordenadaX == 50:
+        alien.coordenadaX = respawn()[0]
+        alien.coordenadaY = respawn()[1]
 
     #Respawn da Municao
     if POSICAO_X_MUNICAO == 1000:
         POSICAO_X_MUNICAO, POSICAO_Y_MUNICAO, GATILHO, VELOCIDADE_X_MUNICAO = respawn_municao()
 
-    if POSICAO_X_ALIEN == 50 or colisao():
-        POSICAO_X_ALIEN = respawn()[0]
-        POSICAO_Y_ALIEN = respawn()[1]
+    if alien.coordenadaX == 50 or colisao():
+        alien.coordenadaX = respawn()[0]
+        alien.coordenadaY = respawn()[1]
 
     #Posicao dos retangulos das imagens (Jogador, Alien, Municao)
     JOGADOR_IMG_RECT.y = POSICAO_Y_JOGADOR
     JOGADOR_IMG_RECT.x = POSICAO_X_JOGADOR
 
-    ALIEN_IMG_RECT.y = POSICAO_Y_ALIEN
-    ALIEN_IMG_RECT.x = POSICAO_X_ALIEN
+    alien.retanguloDaImagem.y = alien.coordenadaY
+    alien.retanguloDaImagem.x = alien.coordenadaX
 
     MUNICAO_RECT.y = POSICAO_Y_MUNICAO
     MUNICAO_RECT.x = POSICAO_X_MUNICAO
         
     #Movimento do Alien
     LARGURA -= 2
-    POSICAO_X_ALIEN -= 1
+    alien.coordenadaX -= 1
 
     #Movimento da Bala
     POSICAO_X_MUNICAO += VELOCIDADE_X_MUNICAO
 
     pygame.draw.rect(TELA, (255,0, 0), JOGADOR_IMG_RECT, 4)
     pygame.draw.rect(TELA, (255,0, 0), MUNICAO_RECT, 4)
-    pygame.draw.rect(TELA, (255,0, 0), ALIEN_IMG_RECT, 4)
+    pygame.draw.rect(TELA, (255,0, 0), alien.retanguloDaImagem, 4)
 
-    TELA.blit(ALIEN_IMG, (POSICAO_X_ALIEN, POSICAO_Y_ALIEN))
+    TELA.blit(alien.imagem, (alien.coordenadaX, alien.coordenadaY))
     TELA.blit(MUNICAO, (POSICAO_X_MUNICAO, POSICAO_Y_MUNICAO))
     TELA.blit(JOGADOR_IMG, (POSICAO_X_JOGADOR, POSICAO_Y_JOGADOR))
 
